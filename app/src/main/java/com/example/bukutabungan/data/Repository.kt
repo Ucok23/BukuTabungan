c617a8acf795b153b7862be59d36773d639473e9
ucok <ulubis98@gmail.com>
2022-08-09 08:16:17 +0700

    change repos

--- app/src/main/java/com/example/bukutabungan/data/Repository.kt
+++ app/src/main/java/com/example/bukutabungan/data/Repository.kt
@@ -3,12 +3,10 @@ package com.example.bukutabungan.data
 import android.app.Application
 import com.example.bukutabungan.data.local.BukuTabunganDB
 import com.example.bukutabungan.data.local.BukuTabunganDao
+import javax.inject.Inject
 
-class Repository(private val application: Application) {
-    private val dao: BukuTabunganDao
+class Repository @Inject constructor(
+    private val bukuTabunganDao: BukuTabunganDao
+) {
 
-    init {
-        val database: BukuTabunganDB = BukuTabunganDB.getInstance(application)
-        dao = database.bukuTabunganDao()
-    }
 }
---
+++ app/src/main/java/com/example/bukutabungan/data/Resource.kt
@@ -0,0 +1,6 @@
+package com.example.bukutabungan.data
+
+sealed class Resource<T> {
+    class Success<T>(val data: T) : Resource<T>()
+    class Failed<T>(val message: String) : Resource<T>()
+}
