22aefb3feffb6b50108e4b8207d633145cffa63a
ucok <ulubis98@gmail.com>
2022-09-11 07:28:52 +0700

    change repos

--- app/src/androidTest/java/com/example/bukutabungan/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/bukutabungan/ExampleInstrumentedTest.kt
@@ -1,25 +1,19 @@
 package com.example.bukutabungan
 
 import android.content.Context
-import androidx.room.Room
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.bukutabungan.data.local.BukuTabunganDB
 import com.example.bukutabungan.data.local.BukuTabunganDao
 import com.example.bukutabungan.data.model.DataTabungan
-import kotlinx.coroutines.runBlocking
 import org.junit.After
-import org.junit.Test
-
-import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
 import java.io.IOException
-import java.lang.Exception
-import java.util.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
